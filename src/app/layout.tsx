f55af0dd96e9c040207114c8b2a3b60336a279ed
R44VC0RP <n01508660@unf.edu>
2024-11-29 18:42:55 -0500

    Update Open Graph metadata and remove unused file

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,7 +4,6 @@ import { CSPostHogProvider } from './providers'
 import { Inter } from 'next/font/google'
 import localFont from 'next/font/local'
 
-
 const ApfelGrotezk = localFont({
   src: [
     {
@@ -68,14 +67,20 @@ export const metadata: Metadata = {
     title: siteConfig.name,
     description: siteConfig.description,
     siteName: siteConfig.name,
+    images: [{
+      url: siteConfig.ogImage,
+      width: 1200,
+      height: 630,
+      alt: siteConfig.name
+    }]
   },
   twitter: {
     card: 'summary_large_image',
     title: siteConfig.name,
     description: siteConfig.description,
     images: [siteConfig.ogImage],
-    creator: '@appliedtrack',
-  },
+    creator: '@' + siteConfig.creator.replace(/\s+/g, '').toLowerCase()
+  }
 }
 
 export default function RootLayout({
